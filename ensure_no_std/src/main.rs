8e0acb6faf3a21ac5deed0ba2f17529e4f7f2c7a
Warlock <internalmike@gmail.com>
2022-06-27 20:53:40 +0300

    0.3.0

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -5,43 +5,50 @@
 
 #![no_std]
 
-use core::alloc::Layout;
-use core::panic::PanicInfo;
-#[cfg(not(windows))]
-use libc::exit;
-use libc_alloc::LibcAlloc;
-use panicking::{panicking, set_panicking_callback};
-#[cfg(windows)]
-use winapi::shared::minwindef::UINT;
-#[cfg(windows)]
-use winapi::um::processthreadsapi::ExitProcess;
-
 #[cfg(windows)]
 #[link(name="msvcrt")]
 extern { }
 
-#[global_allocator]
-static ALLOCATOR: LibcAlloc = LibcAlloc;
+mod no_std {
+    use core::alloc::Layout;
+    use core::panic::PanicInfo;
+    #[cfg(not(windows))]
+    use libc::exit;
+    use libc_alloc::LibcAlloc;
+    #[cfg(windows)]
+    use winapi::shared::minwindef::UINT;
+    #[cfg(windows)]
+    use winapi::um::processthreadsapi::ExitProcess;
 
-#[cfg(windows)]
-unsafe fn exit(code: UINT) -> ! {
-    ExitProcess(code);
-    loop { }
+    #[global_allocator]
+    static ALLOCATOR: LibcAlloc = LibcAlloc;
+
+    #[cfg(windows)]
+    unsafe fn exit(code: UINT) -> ! {
+        ExitProcess(code);
+        loop { }
+    }
+
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        unsafe { exit(99) }
+    }
+
+    #[no_mangle]
+    fn rust_oom(_layout: Layout) -> ! {
+        unsafe { exit(98) }
+    }
+
+    #[no_mangle]
+    extern fn rust_panicking() -> bool {
+        false
+    }
 }
 
-#[panic_handler]
-pub extern fn panic(_info: &PanicInfo) -> ! {
-    unsafe { exit(99) }
-}
-
-#[no_mangle]
-pub fn rust_oom(_layout: Layout) -> ! {
-    unsafe { exit(98) }
-}
+use panicking::panicking;
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    set_panicking_callback(|| false);
     assert!(!panicking());
     0
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(feature="abort", feature(panic_abort))]
+
 #![deny(warnings)]
 #![doc(test(attr(deny(warnings))))]
 #![doc(test(attr(allow(dead_code))))]
@@ -8,24 +10,30 @@
 #[cfg(feature="std")]
 extern crate core;
 
-use core::mem::transmute;
-use core::sync::atomic::{AtomicUsize, Ordering};
+#[cfg(feature="abort")]
+extern crate panic_abort;
 
-static PANICKING_CALLBACK: AtomicUsize = AtomicUsize::new({
-    let no_callback: Option<fn() -> bool> = None;
-    unsafe { transmute(no_callback) }
-});
-
-#[cfg(not(feature="std"))]
-#[inline]
-fn std_panicking() -> bool {
-    false
+#[cfg(all(not(feature="abort"), not(feature="std")))]
+extern {
+    fn rust_panicking() -> bool;
 }
 
-#[cfg(feature="std")]
+#[cfg(all(not(feature="abort"), not(feature="std")))]
 #[inline]
-fn std_panicking() -> bool {
+fn panicking_raw() -> bool {
+    unsafe { rust_panicking() }
+}
+
+#[cfg(all(feature="std", not(feature="abort")))]
+#[inline]
+fn panicking_raw() -> bool {
     std::thread::panicking()
+}
+
+#[cfg(feature="abort")]
+#[inline]
+fn panicking_raw() -> bool {
+    false
 }
 
 /// Determines whether the current thread is unwinding because of panic.
@@ -37,21 +45,7 @@ fn std_panicking() -> bool {
 /// a final application developer: they are supposed to use
 /// the [`set_panicking_callback`] function
 /// to provide method for unwinding detecting.
+#[inline]
 pub fn panicking() -> bool {
-    let panicking = PANICKING_CALLBACK.load(Ordering::Relaxed);
-    let panicking: Option<fn() -> bool> = unsafe { transmute(panicking) };
-    if let Some(panicking) = panicking {
-        panicking()
-    } else {
-        std_panicking()
-    }
-}
-
-/// Allows an application that uses the `no_std` environment
-/// and custom panic handling system with stack unwinding
-/// to inform libraries if the stack is currently unwinding due to panic.
-pub fn set_panicking_callback(panicking: fn() -> bool) {
-    let panicking: Option<fn() -> bool> = Some(panicking);
-    let panicking = unsafe { transmute(panicking) };
-    PANICKING_CALLBACK.store(panicking, Ordering::Relaxed);
+    panicking_raw()
 }
